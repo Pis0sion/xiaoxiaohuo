a09e61c13a814e3e9820a62e1d43e68f7a6aefb6
pis0sion <zihuang2010@126.com>
2019-07-01 18:38:10 +0800

    init

--- application/api/repositories/IntegralRepositories.php
+++ application/api/repositories/IntegralRepositories.php
@@ -14,7 +14,7 @@ class IntegralRepositories
      */
     public function proList($malls)
     {
-        $list = $malls->field('good_name,goods_price,goods_integral,goods_img')->paginate(10);
+        $list = $malls->field('goods_name,goods_price,goods_integral,goods_img')->paginate(10);
         return Utils::renderJson($list);
     }
 
