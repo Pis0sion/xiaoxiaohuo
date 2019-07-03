250ffa10aeba649c7f8dbe739b5ca1464b573c87
pis0sion <zihuang2010@126.com>
2019-07-03 10:15:13 +0800

    init

--- application/api/repositories/OrdersByIntegralRepositories.php
+++ application/api/repositories/OrdersByIntegralRepositories.php
@@ -10,6 +10,7 @@ use app\common\model\IntegralMalls;
 use app\common\model\OrdersByIntegral;
 use app\common\validate\PayOrdersValidate;
 use app\lib\exception\ParameterException;
+use think\Db;
 use think\facade\Log;
 use think\Request;
 
@@ -37,30 +38,38 @@ class OrdersByIntegralRepositories
         $goods_id = $order->hasManyIntegralGoods->goods_id ;
         //  购买数量
         $purchase_count = $order->hasManyIntegralGoods->num ;
-        //  查询该商品的库存量
-        $stock = IntegralMalls::where("goods_id",$goods_id)->lock(true)->value('goods_stock');
-        //  检测库存量
-        $isEnough($stock,$purchase_count);
-        //  查询该用户积分
-        $userIntegral = Accounts::where('uid',$order->user_id)->lock(true)->value('ua_integral_value');
-        //  检测积分
-        $isEnough($userIntegral,$order->order_integral);
-        //  吊起支付
-        $payUrl = (new PayService())->payAction($request->type , $order);
+        Db::startTrans();
+        try{
+            //  查询该商品的库存量
+            $stock = IntegralMalls::where("goods_id",$goods_id)->lock(true)->value('goods_stock');
+            //  检测库存量
+            $isEnough($stock,$purchase_count);
+            //  查询该用户积分
+            $userIntegral = Accounts::where('uid',$order->user_id)->lock(true)->value('ua_integral_value');
+            //  检测积分
+            $isEnough($userIntegral,$order->order_integral);
+            //  吊起支付
+            $payUrl = (new PayService())->payAction($request->type , $order);
 
-        if($payUrl) {
-            // 减库存
-            IntegralMalls::where("goods_id",$goods_id)->setField('goods_stock',bcsub($stock,$purchase_count,2));
-            // 减去积分
-            Accounts::where('uid',$order->user_id)->setField('ua_integral_value',bcsub($userIntegral,$order->order_integral,2));
-            // TODO:  添加日志
+            if($payUrl) {
+                // 减库存
+                IntegralMalls::where("goods_id",$goods_id)->setField('goods_stock',bcsub($stock,$purchase_count,2));
+                // 减去积分
+                Accounts::where('uid',$order->user_id)->setField('ua_integral_value',bcsub($userIntegral,$order->order_integral,2));
+                // TODO:  添加日志
 
-            //  订单写入队列
-            $this->writeQueue(compact('order_sn'));
+                //  订单写入队列
+                $this->writeQueue(compact('order_sn'));
 
-            return Utils::renderJson(compact('payUrl'));
+                Db::commit();
+                return Utils::renderJson(compact('payUrl'));
 
+            }
+        }catch (\Throwable $e) {
+
+            Db::rollback();
         }
+
 
         throw new ParameterException(['msg' => '支付失败']);
     }
@@ -71,16 +80,23 @@ class OrdersByIntegralRepositories
     private function writeQueue($data)
     {
         $stomp = new \Stomp('tcp://47.95.9.36:61613','pis0sion','zihuang2010=-0');
+
         $queue = "/queue/orderIsPay" ;
+
         try {
             $stomp->begin("trans");
+
             $stomp->send($queue, json_encode($data), [
                 'PERSISTENT' => 'true',
                 'AMQ_SCHEDULED_DELAY' => 20 * 60 * 1000,
             ]);
+
             $stomp->commit("trans");
+
         }catch (\Throwable $e){
+
             $stomp->abort('trans');
+
             $this->LogError(json_encode($data));
         }
     }
