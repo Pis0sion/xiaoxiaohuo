03144adbe4eee9c242034528307fc4c006ee3608
pis0sion <zihuang2010@126.com>
2019-07-03 10:19:10 +0800

    init

--- application/api/repositories/OrdersByIntegralRepositories.php
+++ application/api/repositories/OrdersByIntegralRepositories.php
@@ -38,8 +38,8 @@ class OrdersByIntegralRepositories
         $goods_id = $order->hasManyIntegralGoods->goods_id ;
         //  购买数量
         $purchase_count = $order->hasManyIntegralGoods->num ;
-        Db::startTrans();
-        try{
+//        Db::startTrans();
+//        try{
             //  查询该商品的库存量
             $stock = IntegralMalls::where("goods_id",$goods_id)->lock(true)->value('goods_stock');
             //  检测库存量
@@ -49,10 +49,13 @@ class OrdersByIntegralRepositories
             //  检测积分
             $isEnough($userIntegral,$order->order_integral);
             //  吊起支付
-            $payUrl = (new PayService())->payAction($request->type , $order);
+         //   $payUrl = (new PayService())->payAction($request->type , $order);
+
+        $payUrl = true ;
 
             if($payUrl) {
                 // 减库存
+                halt(bcsub($stock,$purchase_count,2));
                 IntegralMalls::where("goods_id",$goods_id)->setField('goods_stock',bcsub($stock,$purchase_count,2));
                 // 减去积分
                 Accounts::where('uid',$order->user_id)->setField('ua_integral_value',bcsub($userIntegral,$order->order_integral,2));
@@ -61,14 +64,15 @@ class OrdersByIntegralRepositories
                 //  订单写入队列
                 $this->writeQueue(compact('order_sn'));
 
-                Db::commit();
+              //  Db::commit();
+
                 return Utils::renderJson(compact('payUrl'));
 
             }
-        }catch (\Throwable $e) {
-
-            Db::rollback();
-        }
+//        }catch (\Throwable $e) {
+//
+//            Db::rollback();
+//        }
 
 
         throw new ParameterException(['msg' => '支付失败']);
