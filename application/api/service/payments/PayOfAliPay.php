3429424adc4e515925f12afe0d705662854d15a8
pis0sion <zihuang2010@126.com>
2019-07-04 14:33:18 +0800

    init

--- application/api/service/payments/PayOfAliPay.php
+++ application/api/service/payments/PayOfAliPay.php
@@ -50,7 +50,7 @@ class PayOfAliPay extends IPayChannels
     public function doNotify(\Closure $success,\Closure $fail)
     {
 
-        die("123456123");
+        die("回调");
 
         $request = $this->gateWay->completePurchase();
         $request->setParams($_POST); //Optional
